3702ba652cddb53c4b98bbe4582ba30ce45ad2a4
michelgirault <michel.girault98@gmail.com>
2025-01-01 12:38:07 +0100

    add vllm

--- vllm-opt/starter.sh
+++ vllm-opt/starter.sh
@@ -3,15 +3,14 @@
 #aws configure set aws_access_key_id $aws_access_key_id
 #aws configure set aws_secret_access_key $aws_secret_access_key
 
-#download models for sd
+#download model
 wget -nc $LLM_MODEL_URL -P $LLM_LOCAL_MODEL_REP
 
 
 #start server
-server/bin/wasmedge --dir .:. --env API_KEY=$LLAMA_API_KEY --nn-preload default:GGML:AUTO:$LLM_LOCAL_MODEL_REP$LLM_MODEL_NAME \
-    /app/server/llama-api-server.wasm --prompt-template $LLM_TEMPLATE \
+vllm serve $LLM_MODEL_ID --dtype auto --api-key $LLAMA_API_KEY \
+    --host 0.0.0.0 \
     --port 4000 \
-    --model-name $LLM_MODEL_NAME \
-    --n-gpu-layers $GPU_LAYER \
-    --ctx-size $CTX \
-    --main-gpu $CUDA
+    --served-model-name $LLM_MODEL_NAME \
+    --quantization $QUANT \
+    --cpu-offload-gb $OFFLOAD_CPU
